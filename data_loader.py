67f9081dac1551a25543a14d3cafc7d89ccc64f9
nkrissetiyanto-png <nkrissetiyanto@me.com>
2025-11-21 16:21:02 +0700

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,15 @@
 import yfinance as yf
 import pandas as pd
 
-def load_data(tickers=["BBNI.JK", "BRIS.JK"], period="1y"):
-    df_list = []
-    for t in tickers:
-        df = yf.download(t, period=period, interval="1d", auto_adjust=False)
-        if df.empty:
-            print(f"⚠️ Data kosong untuk {t}, dilewati.")
-            continue
-        df.dropna(inplace=True)
-        df["Ticker"] = t
-        df_list.append(df)
-    if not df_list:
-        raise ValueError("❌ Tidak ada data yang berhasil diambil!")
-    return pd.concat(df_list)
+def load_data(ticker="BBNI.JK", period="1y"):
+    df = yf.download(ticker, period=period, interval="1d", auto_adjust=False)
+
+    if df.empty:
+        raise ValueError(f"❌ Data kosong untuk {ticker}")
+
+    df = df.dropna()
+
+    # pastikan kolom Datetime ada
+    df.reset_index(inplace=True)
+
+    return df
